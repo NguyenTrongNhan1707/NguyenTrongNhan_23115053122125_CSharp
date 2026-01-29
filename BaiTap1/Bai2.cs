7ca81924c165d90d5e35c9e4ddbe9e0e243c72ce
NguyenTrongNhan1707 <nguyentrongnhan17072005@gmail.com>
2026-01-29 08:29:15 +0700

    Bai2 BTChuoi

---
+++ BaiTap1/Bai2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap1
+{
+    using System;
+
+    class Bai2
+    {
+        static void Main()
+        {
+            Console.Write("Nhap chuoi: ");
+            string s = Console.ReadLine();
+
+            if (s == null)
+            {
+                Console.WriteLine("So tu: 0");
+                return;
+            }
+
+            s = s.Trim();
+
+            if (s == " ")
+            {
+                Console.WriteLine("So tu: 0");
+                return;
+            }
+
+            string[] arr = s.Split(new char[] { ' ' }, 
+                StringSplitOptions.RemoveEmptyEntries);
+
+            int dem = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                dem++;
+            }
+
+            Console.WriteLine("So tu: " + dem);
+        }
+    }
+
+}
